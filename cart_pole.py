d7c94f1fa524c93030c0ef48013bef79358bff44
nicholas <nicholas@explore-ai.net>
2018-12-26 12:07:14 +0200

    Fixed steepest ascent implementation

--- cart_pole.py
+++ cart_pole.py
@@ -14,8 +14,7 @@ print('observation space:', env.observation_space)
 print('action space:', env.action_space)
 
 
-class Policy():
-
+class Policy:
     def __init__(self, s_size=4, a_size=2):
         # weights for simple linear policy: state_space x action_space
         self.w = 1e-4 * np.random.rand(s_size, a_size)
@@ -41,6 +40,10 @@ def hill_climbing(policy, n_episodes=1000, max_t=1000, gamma=1.0, print_every=10
         gamma (float): discount rate
         print_every (int): how often to print average score (over last 100 episodes)
         noise_scale (float): standard deviation of additive noise
+
+    Returns
+    =======
+        scores (list): total rewards achieved for each episode
     """
     scores_deque = deque(maxlen=100)
     scores = []
@@ -75,14 +78,14 @@ def hill_climbing(policy, n_episodes=1000, max_t=1000, gamma=1.0, print_every=10
                 i_episode, np.mean(scores_deque)))
         if np.mean(scores_deque) >= 195.0:
             print('Environment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
-                i_episode - 100, np.mean(scores_deque)))
+                i_episode, np.mean(scores_deque)))
             policy.w = best_w
             break
 
     return scores
 
 
-def steepest_ascent(policy, n_episodes=1000, max_t=1000, gamma=1.0, print_every=100, noise_scale=1e-2, neighbour_size=10):
+def steepest_ascent(policy, n_episodes=1000, max_t=1000, gamma=1.0, print_every=100, noise_scale=1e-2, neighbour_size=5):
     """Implementation of steepest ascent adaptive noise scaling.
 
     Params
@@ -97,25 +100,26 @@ def steepest_ascent(policy, n_episodes=1000, max_t=1000, gamma=1.0, print_every=
     scores = []
     best_R = -np.Inf
     best_w = policy.w
-    possible_w = [policy.w for _ in range(neighbour_size)]
+    possible_w = [policy.w]
     for i_episode in range(1, n_episodes + 1):
         rewards = []
-        idx = 0
-        for p, w in enumerate(possible_w):
-            current_rewards = []
+        tmp_best_R = -np.Inf
+        tmp_best_w = policy.w
+        for w in possible_w:
+            tmp_rewards = []
             state = env.reset()
             policy.w = w
             for t in range(max_t):
                 action = policy.act(state)
                 state, reward, done, _ = env.step(action)
-                current_rewards.append(reward)
+                tmp_rewards.append(reward)
                 if done:
                     break
-            if sum(current_rewards) > sum(rewards):
-                rewards = current_rewards
-                idx = p
-
-        policy.w = possible_w[idx]
+            if sum(tmp_rewards) > tmp_best_R:
+                tmp_best_R = sum(tmp_rewards)
+                tmp_best_w = w
+                rewards = tmp_rewards.copy()
+        policy.w = tmp_best_w
         scores_deque.append(sum(rewards))
         scores.append(sum(rewards))
 
@@ -127,22 +131,22 @@ def steepest_ascent(policy, n_episodes=1000, max_t=1000, gamma=1.0, print_every=
             best_w = policy.w
             noise_scale = max(1e-3, noise_scale / 2)
             possible_w = [policy.w + noise_scale *
-                          np.random.rand(*policy.w.shape) for _ in range(neighbour_size)]
+                          np.random.rand(*policy.w.shape) for _ in range(neighbour_size + 1)]
         else:  # did not find better weights
             noise_scale = min(2, noise_scale * 2)
             possible_w = [best_w + noise_scale *
-                          np.random.rand(*policy.w.shape) for _ in range(neighbour_size)]
+                          np.random.rand(*policy.w.shape) for _ in range(neighbour_size + 1)]
 
         if i_episode % print_every == 0:
             print('Episode {}\tAverage Score: {:.2f}'.format(
                 i_episode, np.mean(scores_deque)))
         if np.mean(scores_deque) >= 195.0:
             print('Environment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(
-                i_episode - 100, np.mean(scores_deque)))
+                i_episode, np.mean(scores_deque)))
             policy.w = best_w
             break
 
-    return scores, policy
+    return scores
 
 
 def display(policy):
@@ -158,7 +162,8 @@ def display(policy):
             break
     env.close()
 
+
 if __name__ == "__main__":
     policy = Policy()
-    scores = hill_climbing(policy)
+    scores = steepest_ascent(policy)
     display(policy)
